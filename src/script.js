50d8a61574429dbac9aa8e4d655b776602e269a0
Feniuk <143640539+Feniuk@users.noreply.github.com>
2026-01-26 11:06:59 +0100

    week2 practice

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("donate_button");
+
+btn.addEventListener("click", function () {
+  window.open(
+    "https://savelife.in.ua/en/donate-en/#donate-army-card-once",
+    "_blank",
+  );
+});
